915ee4ab3d3b7f0163f77f8d85de095c7bc46a9c
Justin Meyer <justinbmeyer@gmail.com>
2016-01-29 14:20:49 -0600

    include steal and inline css

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ var stealTools = require("steal-tools");
 var buildPromise = stealTools.build({
   config: __dirname + "/package.json!npm"
 }, {
+  bundleSteal: true,
   bundleAssets: {
     infer: true,
     glob: ["node_modules/opensourced-bithub/images/**/*","node_modules/font-awesome/fonts/**/*"]

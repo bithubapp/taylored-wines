5c2f6ca929d17ec04355655eab171a981368731a
Justin Meyer <justinbmeyer@gmail.com>
2016-01-29 14:02:40 -0600

    minify

--- build.js
+++ build.js
@@ -3,7 +3,6 @@ var stealTools = require("steal-tools");
 var buildPromise = stealTools.build({
   config: __dirname + "/package.json!npm"
 }, {
-  minify: false,
   bundleAssets: {
     infer: true,
     glob: ["node_modules/opensourced-bithub/images/**/*","node_modules/font-awesome/fonts/**/*"]
